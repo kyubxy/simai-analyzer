2ddf4e7a57dbe8cfeb3086e411d1c14de8e2c596
kyubxy <mentairicebowl@gmail.com>
2025-01-26 18:55:33 +1100

    use a pipe

--- src/simai.ts
+++ src/simai.ts
@@ -1,8 +1,16 @@
-import { Chart, MaiChart } from "./chart"
-import { genAbsyn } from "./deserialization/absyn"
-import { parse } from "../lib/parser"
+import { Chart, MaiChart } from "./chart";
+import { genAbsyn } from "./deserialization/absyn";
+import { parse } from "../lib/parser";
+import { pipe } from "fp-ts/lib/function";
 
-export const deserializeChart = (data: string): Chart => genAbsyn(parse(data))
+export const deserializeChart = (data: string): Chart => 
+  pipe(
+    data,
+    parse,
+    genAbsyn
+  )
 
-export const serializeChart = (chart: MaiChart): string => 
-  ""
+// TODO:
+export const serializeChart = (chart: MaiChart): string => {
+  throw new Error("Not implemented yet.");
+};
