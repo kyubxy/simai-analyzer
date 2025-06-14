360efda8e14164ed19e69c37a2e14ed3096f213b
Justin <mentairicebowl@gmail.com>
2025-06-14 18:04:09 +1000

    1.0.13
    remove

--- test/simai.test.ts
+++ test/simai.test.ts
@@ -64,11 +64,4 @@ E4/8,,7/B3,7,B2,6,E2,6,E4,5,B3,5,B2/5,5,6/E2,6,
     expect(chart!.author).toBe("kyubey");
     expect(chart!.levels[difficulties.master].level).toBe("14");
   });
-
-  test("stuff", () => {
-    const data = luciaRemas;
-    const result = deserializeSingle(data).chart;
-    console.log("ya", result);
-    expect(true).toBe(true);
-  });
 });
